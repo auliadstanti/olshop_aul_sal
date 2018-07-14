b2aa29d3a113b623a9cd382e9ae053dd8972ebde
salsanaliafthin <35424301+salsanaliafthin@users.noreply.github.com>
2018-07-14 18:09:11 +0700

    database

--- database/aul_sal.sql
+++ database/aul_sal.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2018 at 05:40 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: 14 Jul 2018 pada 11.04
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,79 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `sepatu`
+--
+
+CREATE TABLE `sepatu` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `merk` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kategori` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `warna` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ukuran_tersedia` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga` int(20) NOT NULL,
+  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `sepatu`
+--
+
+INSERT INTO `sepatu` (`id`, `nama`, `merk`, `kategori`, `warna`, `ukuran_tersedia`, `stok`, `harga`, `gambar`, `tanggal`) VALUES
+(2, '1', '1', '1', '1', 'XL,L,M', 1, 1, 'Code1.png', '2018-06-15 00:12:38');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `nomor` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `fk_users` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `nomor`, `tanggal`, `status`, `fk_users`) VALUES
+(1, 'TRN00001', '2018-06-14 00:00:00', 1, 2),
+(2, 'TRN00002', '2018-06-14 00:00:00', 1, 2),
+(3, 'TRN00003', '2018-06-14 00:00:00', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `id` int(11) NOT NULL,
+  `fk_transaksi` int(11) NOT NULL,
+  `fk_sepatu` int(11) NOT NULL,
+  `ukuran` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `transaksi_detail`
+--
+
+INSERT INTO `transaksi_detail` (`id`, `fk_transaksi`, `fk_sepatu`, `ukuran`, `jumlah`) VALUES
+(1, 1, 2, 'XL', 4),
+(2, 2, 2, 'XL', 4),
+(3, 3, 2, 'XL', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -36,16 +108,40 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `level`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
-(2, 'user', 'user', 'user');
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
+(3, '1', 'c4ca4238a0b923820dcc509a6f75849b', 'admin'),
+(4, '2', 'c81e728d9d4c2f636f067f89cc14862c', 'admin'),
+(6, '4', 'a87ff679a2f3e71d9181a67b7542122c', 'admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `sepatu`
+--
+ALTER TABLE `sepatu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_users` (`fk_users`);
+
+--
+-- Indexes for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_sepatu` (`fk_sepatu`),
+  ADD KEY `fk_transaksi` (`fk_transaksi`);
 
 --
 -- Indexes for table `users`
@@ -58,10 +154,41 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `sepatu`
+--
+ALTER TABLE `sepatu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`fk_users`) REFERENCES `users` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD CONSTRAINT `transaksi_detail_ibfk_1` FOREIGN KEY (`fk_sepatu`) REFERENCES `sepatu` (`id`),
+  ADD CONSTRAINT `transaksi_detail_ibfk_2` FOREIGN KEY (`fk_transaksi`) REFERENCES `transaksi` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
